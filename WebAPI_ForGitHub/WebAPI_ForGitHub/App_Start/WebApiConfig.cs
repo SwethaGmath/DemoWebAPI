dfbb4ac31e4e2e4ccc19510eb0befd9d8b7aa3a4
SWETHA GACHCHINAMATH <swetha.gachchinamath@gmail.com>
2019-03-07 15:12:23 +1300

    Added Global exception handler and logger

--- WebAPI_ForGitHub/WebAPI_ForGitHub/App_Start/WebApiConfig.cs
+++ WebAPI_ForGitHub/WebAPI_ForGitHub/App_Start/WebApiConfig.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
 using Unity;
 using Unity.Lifetime;
 using WebAPI_ForGitHub.DataAccessLayer;
+using WebAPI_ForGitHub.Helper;
 using WebAPI_ForGitHub.Services;
 
 namespace WebAPI_ForGitHub
@@ -17,6 +19,8 @@ namespace WebAPI_ForGitHub
             container.RegisterType<IProductsDAL, ProductsDAL>(new HierarchicalLifetimeManager());
             container.RegisterType<IProductServices, ProductServices>(new HierarchicalLifetimeManager());
             config.DependencyResolver = new UnityResolver(container);
+            config.Services.Add(typeof(IExceptionLogger), new GLobalExceptionLogger());
+            config.Services.Replace(typeof(IExceptionHandler), new GlobalExceptionHandler());
             // Web API configuration and services
 
             // Web API routes
