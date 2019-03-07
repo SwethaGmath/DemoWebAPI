086ae9949b40fd2ff596c2427769700b53b26c0f
SWETHA GACHCHINAMATH <swetha.gachchinamath@gmail.com>
2019-03-07 15:06:41 +1300

    Adding Global exception handler and logger

---
+++ WebAPI_ForGitHub/WebAPI_ForGitHub/Helper/GLobalExceptionLogger.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Http.ExceptionHandling;
+
+namespace WebAPI_ForGitHub.Helper
+{
+    public class GLobalExceptionLogger : ExceptionLogger
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public override void Log(ExceptionLoggerContext context)
+        {
+            var log = context.Exception.ToString();
+            Trace.TraceError(context.ExceptionContext.Exception.ToString());
+            //Write the exception to your logs
+            logger.Error(context.Exception, "Oops, something unexpected happened!!");
+        }
+    }
+}
---
+++ WebAPI_ForGitHub/WebAPI_ForGitHub/Helper/GlobalExceptionHandler.cs
@@ -0,0 +1,30 @@
+﻿using NLog;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+using System.Threading;
+using System.Net;
+using System.Web.Http.Results;
+using System.Net.Http;
+
+namespace WebAPI_ForGitHub.Helper
+{
+    public class GlobalExceptionHandler : ExceptionHandler
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public override Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+        {
+            logger.Error(context.Exception, context.ExceptionContext.Exception.StackTrace);
+            var response = context.Request.CreateResponse(HttpStatusCode.InternalServerError,
+                new
+                {
+                    Message = "Oops!! An unexpected error occured"
+                });
+
+            context.Result = new ResponseMessageResult(response);
+            return Task.FromResult<object>(response);
+        }
+
+    }
+}
